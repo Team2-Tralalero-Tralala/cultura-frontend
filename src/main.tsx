a00bb9ed08b5072b33d008d95906e6b05bdafd1e
Natthanan04 <natthanansirisurayut@gmail.com>
2025-08-27 10:57:49 +0700

    feat(router): install react-router and add basic routes in main

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div className="font-sarabun">
-      <App />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          {/* <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} /> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   </StrictMode>
 );

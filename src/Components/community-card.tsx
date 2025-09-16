24c6383c3d8d54bfe331babbb4380bf80a645b03
66160108-camp-66 <66160108@go.buu.ac.th>
2025-09-16 11:19:19 +0700

    community-card COMPLETE!
    อัพเดต
    - แก้ไขแสดงรูปภาพ
    - แก้ไขชื่อไฟล์ App เป็น community-card
    - แก้ไขการนำเข้าใช้รูปภาพ
    
    - เพิ่ม folder components
    - เพิ่ม folder image
    - เพิ่มความคิดเห็น

--- src/Components/community-card.tsx
+++ src/Components/community-card.tsx
@@ -1,16 +1,22 @@
+import profileDefault from "./image/150fa8800b0a0d5633abc1d1c4db3d87.jpg";
+// นำเข้ารูปภาพจากไฟล์ image ใน Components
+
+// function สำหรับกรอกข้อมูลตัวอย่าง แสดงหน้าเว็ป
 function App() {
   return (
     <CommunityCard
       name="ชุมชนบ้านห้วยไล่วัดกลางดอน"
-      imageUrl="https://scontent.futp1-1.fna.fbcdn.net/v/t39.30808-6/296108082_1205436640243819_6795992127038476452_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=OVTRUSnOooUQ7kNvwHMrG_T&_nc_oc=AdkMuOA2r5RWGQ1AfjqJFz1yqbh8-iI0y_m1GQsEwkYvxyrz_oxd3zMKrNTrOgJTV5UfSLMSevu9EBD2zmW_yQK-&_nc_zt=23&_nc_ht=scontent.futp1-1.fna&_nc_gid=O85krrkcvON_OPHg6PZi2Q&oh=00_AfabzRnFZ4n5AOZifK4KwV_RJNzRAAUo-bp-D1LTmHoyAg&oe=68C213BA"
+      imageUrl={profileDefault}
     />
   );
 }
+
+// function สำหรับสร้าง card
 // Child component
 function CommunityCard(props: { name: string; imageUrl: string }) {
   return (
     <div className="flex flex-row justify-center mt-10">
-      <div className="mx-2 w-40 h-50 bg-white rounded-xl border overflow-hidden text-center p-4">
+      <div className="mx-2 w-40 h-50 bg-white rounded-xl border border-gray-200 overflow-hidden text-center p-4">
         <img
           className="mx-auto h-24 w-24 rounded-full bg-gray-300"
           src={props.imageUrl}
--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./App.tsx";
+import App from "./Components/community-card.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

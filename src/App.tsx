7f6fc5585d2ebf82ef7150d8f7e7c88f85c64f5a
Natthanan04 <natthanansirisurayut@gmail.com>
2025-09-17 22:30:30 +0700

    feat(routes): implement routing for login, registration, and password

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,34 @@
+import { Route, Routes, Navigate } from "react-router";
+// import ProtectedRoute from "./Libs/ProtectedRoute";
+import RoleRedirect from "./Libs/RoleRedirect";
+
+import LoginAdmin from "./Pages/LoginAdmin.tsx";
+import Register from "./Pages/Register.tsx";
+import ForgotPassword from "./Pages/ForgotPassword.tsx";
+import LoginTourist from "./Pages/LoginTourist.tsx";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        {/* public */}
+        <Route path="/login" element={<LoginTourist />} />
+        <Route path="admin/login" element={<LoginAdmin />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        {/* root -> เด้งตาม role */}
+        <Route index element={<RoleRedirect />} />
+
+        {/* private routes */}
+        {/* <Route
+          element={<ProtectedRoute allow={["member", "admin", "superadmin"]} />}
+        >
+          <Route path="/admin/home" element={<Admin />} />
+        </Route> */}
+        {/* fallback */}
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router";
+import { AuthProvider } from "./Libs/AuthProviders";
+import App from "./App";
+
 import "./index.css";
-import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <div className="font-sarabun">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          {/* <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} /> */}
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
     </div>
   </StrictMode>
 );

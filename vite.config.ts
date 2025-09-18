2cb969c655585a0600bf584caf4f10cc12b39f33
TorlapSayHi <torlap.ritchai@gmail.com>
2025-09-18 19:50:45 +0700

    fix(vite): restore base path configuration in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/~66160354/',   // << สำคัญ
   plugins: [react(), tailwindcss()],
 });

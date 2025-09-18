15bb59b57df5c1d924c911f211b85fbc21c407f8
TorlapSayHi <torlap.ritchai@gmail.com>
2025-09-18 20:11:39 +0700

    fix(vite): remove base path configuration from vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/~66160354/',   // << สำคัญ
   plugins: [react(), tailwindcss()],
 });

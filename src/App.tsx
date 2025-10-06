270dec10a24ef1f43747023ce476730987576bed
TeerapolGOD <teerapol194@gmail.com>
2025-10-07 01:24:27 +0700

    feat(pagination): update App component to include onQueryChange prop for pagination components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
 import React from "react";
-import TablePagination from './Components/Pagination/TablePagination';
-import PaginationRoundedForCardPackage  from './Components/Pagination/PaginationRoundedForCardPackage';
+import TablePagination from "./Components/Pagination/TablePagination";
+import PaginationRoundedForCardPackage from "./Components/Pagination/PaginationRoundedForCardPackage";
+import PaginationRoundedForCardCommunity from "./Components/Pagination/PaginationRoundedForCardCommunity";
 
 function App() {
-  const totalData = 50;
+  const totalData = 100; // จำลองข้อมูลทั้งหมด
+
+  // รับ page, limit ที่เปลี่ยนแปลงจาก pagination components
+  const handleQueryChange = (query: { page: number; limit: number }) => { 
+    alert(`page: ${query.page}, limit: ${query.limit}`);
+  };
+
   return (
-    <div >
-      <TablePagination totalData={totalData}/>
-      <PaginationRoundedForCardPackage totalData={totalData}/>
+    <div>
+      <TablePagination totalData={totalData} onQueryChange={handleQueryChange} />
+      <br />
+      <PaginationRoundedForCardPackage totalData={totalData} onQueryChange={handleQueryChange} />
+      <br />
+      <PaginationRoundedForCardCommunity totalData={totalData} onQueryChange={handleQueryChange} />
+      <br />
     </div>
   );
 }

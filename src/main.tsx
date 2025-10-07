c2cb1695ebd3fcf533d8b0e55bf85b837b3b16a5
66160108-camp-66 <66160108@go.buu.ac.th>
2025-10-08 00:48:00 +0700

    feature(community-card): add CommunityCard component with default profile image
    แก้ตาม Standard

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Route, Routes } from "react-router";
-import App from "./Components/community-card.tsx";
+import App from "./Components/CommunityCard.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>

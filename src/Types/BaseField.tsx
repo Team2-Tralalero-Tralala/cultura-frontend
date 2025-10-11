ac406db60e7a907456dc0efc4c8b06736030e3ca
Natthanan04 <natthanansirisurayut@gmail.com>
2025-10-11 21:51:20 +0700

    fix(basefield): value to any

--- src/Types/BaseField.tsx
+++ src/Types/BaseField.tsx
@@ -21,7 +21,7 @@ export type BaseFieldProps = {
   required?: boolean;
   placeholder?: string;
   type?: string;
-  value?: string;
+  value?: any;
   onChange?: (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => void;

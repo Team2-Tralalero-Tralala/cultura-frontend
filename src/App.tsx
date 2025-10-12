cc055b787ed6eed2e329c866a6f8dfc0b80b9106
tawan475 <43491902+tawan475@users.noreply.github.com>
2025-10-12 22:21:08 +0700

    revert App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,9 @@
-import { useState } from "react";
-import { Combobox } from "./Components/ComboBox";
-
 function App() {
-    const [selectedLanguage, setSelectedLanguage] = useState("");
-    const languages = Array.from({ length: 20 }, (_, i) => ({
-        value: i % 2 === 0 ? `lang${i}` : `lang${i}`,
-        label: i % 2 === 0 ? `Language ${i}` : `ภาษา ${i}`,
-    }));
-    return (
-        <div className="flex items-center justify-center min-h-screen">
-            <div className="m-auto p-4 border rounded-xl shadow-lg items-center justify-center w-fit">
-                <div>
-                  ภาษาที่เลือก: {selectedLanguage ? languages.find(lang => lang.value === selectedLanguage)?.label : "ยังไม่ได้เลือก"}
-                </div>
-                <Combobox title="ภาษา" value={selectedLanguage} items={languages} onChange={setSelectedLanguage} />
-            </div>
-        </div>
-    );
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;

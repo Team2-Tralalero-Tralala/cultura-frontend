2bb4eeb4345bddd5975ea5507de7414cb717fbf9
TorlapSayHi <torlap.ritchai@gmail.com>
2025-10-20 23:21:27 +0700

    feat(package-requests): Add ManagePackageRequestPage and integrate RejectModal for handling package approval requests

--- src/Layouts/SuperAdmin/SuperAdminRoutes.tsx
+++ src/Layouts/SuperAdmin/SuperAdminRoutes.tsx
@@ -6,6 +6,7 @@ import { Routes, Route } from "react-router-dom";
 import ManageCommunitySuperAdmin from "@/Pages/SuperAdmin/ManageCommunitySuperAdmin";
 import CommunityDetailSuperAdmin from "@/Pages/SuperAdmin/CommunityDetailSuperAdmin";
 import AuthentionLogSuperAdmin from "@/Pages/SuperAdmin/AuthentionLogSuperAdmin";
+import ManagePackageRequestPage from "@/Pages/SuperAdmin/ManagePackageRequestPage";
 
 export default function SuperAdminRoutes() {
   return (
@@ -20,6 +21,7 @@ export default function SuperAdminRoutes() {
         element={<CommunityDetailSuperAdmin />}
       />
       <Route path="logs" element={<AuthentionLogSuperAdmin />} />
+      <Route path="package-requests" element={<ManagePackageRequestPage />} />
     </Routes>
   );
 }

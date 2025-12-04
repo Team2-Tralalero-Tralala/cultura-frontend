df56bc6ea970bb77241b05dd64237f92c17d095c
Pangwaree <66160362@go.buu.ac.th>
2025-12-04 14:42:59 +0700

    feat : Add member package history page and service

--- src/Layouts/Member/MemberRoutes.tsx
+++ src/Layouts/Member/MemberRoutes.tsx
@@ -10,12 +10,14 @@
 import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import ChangePasswordPage from "@/Pages/SuperAdmin/ChangePasswordPage";
+import PackageHistoryMember from '@/Pages/Member/HistoryPackageMember';
 
 export default function MemberRoutes() {
   return (
     <Routes>
       
       <Route path="account/change-password/own" element={<ChangePasswordPage />} />
+      <Route path="packages/done" element={<PackageHistoryMember />} />
     </Routes>
   );
 }

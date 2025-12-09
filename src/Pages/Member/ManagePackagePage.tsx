23c2cf11061883820d52b0774f10bb53f627b277
TorlapSayHi <torlap.ritchai@gmail.com>
2025-12-09 23:01:08 +0700

    แก้ไขปุ่มเพิ่มแพ็กเกจ

--- src/Pages/Member/ManagePackagePage.tsx
+++ src/Pages/Member/ManagePackagePage.tsx
@@ -20,6 +20,7 @@ import axios from "axios";
 import Button from "@/Components/Button";
 import { Modal } from "@/Components/Modal/Modal";
 import Breadcrumb from "@/Components/BreadcrumbNavigation";
+import { Icon } from "@iconify/react";
 
 const apiUrl = import.meta.env.VITE_API_URL;
 
@@ -313,11 +314,16 @@ export default function ManagePackageSuperAdmin() {
           </div>
 
           <div className="ml-auto flex items-center gap-3">
-            <Button type="confirm-admin" onClick={goToApprovalRequests}>
-              คำขออนุมัติ
-            </Button>
             <Button type="confirm-admin" onClick={goToCreatePackage}>
-              <span className="whitespace-nowrap">สร้างแพ็กเกจ</span>
+              <div className="flex items-center justify-center gap-2 px-1">
+                <Icon
+                  icon="material-symbols:add-rounded"
+                  className="text-2xl" // ปรับขนาดไอคอนประมาณ 24px
+                />
+                <span className="whitespace-nowrap font-medium text-lg pb-0.5">
+                  เพิ่มแพ็กเกจ
+                </span>
+              </div>
             </Button>
           </div>
         </div>

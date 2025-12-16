da2518045e0cdbf38d6dc05035bf0c90dfd99207
TeerapolGOD <teerapol194@gmail.com>
2025-12-16 18:03:45 +0700

    fix: comment service ตาม codinf standard

--- src/Services/store-service.ts
+++ src/Services/store-service.ts
@@ -108,15 +108,11 @@ export async function deleteStore(storeId: number) {
   return api.delete(`/shared/store/${storeId}/delete`);
 }
 
-/**
- * ฟังก์ชัน : getStoreWithOtherStoresInCommunity
- * คำอธิบาย : ฟังก์ชันสำหรับดึงข้อมูลร้านค้าตาม communityId และ storeId
- * Input :
- *   - communityId (number) : ID ของชุมชน
- *   - storeId (number) : ID ของร้านค้า
- * Output :
- *   - Promise จากการเรียก API
- */
+/*
+ * คำอธิบาย : ฟังก์ชันสำหรับดึงข้อมูลร้านค้าตามและร้านค้าอื่นๆในชุมชนเดียวกัน
+ * Input : communityId, storeId
+ * Output : ข้อมูลร้านค้าตามและร้านค้าอื่นๆในชุมชนเดียวกัน
+*/
 export async function getStoreWithOtherStoresInCommunity(communityId: number, storeId: number, page: number, limit: number) {
   const response = await axios.get(`${apiUrl}/shared/community/${communityId}/store/${storeId}`,
     {

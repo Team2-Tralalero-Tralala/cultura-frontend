d013f54cee4acb54e080f892890dcee61de8e166
TorlapSayHi <torlap.ritchai@gmail.com>
2026-03-05 12:54:14 +0700

    แก้ไขจำนวนการจอง

--- src/Pages/Admin/ManagePackagePage.tsx
+++ src/Pages/Admin/ManagePackagePage.tsx
@@ -136,7 +136,7 @@ export default function ManagePackagePage() {
             packageItem?.published === true ||
             packageItem?.isPublished === true,
           statusApprove: packageItem?.statusApprove ?? null,
-          bookedCount: packageItem?.bookingHistories?.length ?? 0,
+          bookedCount: packageItem?.bookedCount ?? 0,
           capacity: packageItem?.capacity ?? 0,
         }),
       );
